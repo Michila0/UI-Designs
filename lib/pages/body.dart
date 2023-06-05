9fd3da4439e134d09691c3988073a694bd0cc43d
Michila <michila.bimsara12@gmail.com>
2023-06-05 15:13:48 +0530

    has bug

--- lib/pages/body.dart
+++ lib/pages/body.dart
@@ -1,2 +1,30 @@
 import 'package:flutter/material.dart';
 
+class Body extends StatefulWidget {
+  const Body({super.key});
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  Widget build(BuildContext context) {
+    //It provides us total height and width
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <widget>[
+          SizedBox(
+            height: size.height,
+            child: Stack(
+              children: <widget>[
+                Container()
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
