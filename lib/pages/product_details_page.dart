d9be91d561d76e6c28b29b3b8b28aa31c18fc830
Michila <michila.bimsara12@gmail.com>
2023-06-05 13:40:39 +0530

    have bug

--- lib/pages/product_details_page.dart
+++ lib/pages/product_details_page.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
 
+// class product_details_page {
+//   final String id, price, title, images, description;
+//   final int rating;
+//
+//   product_details_page({
+//    this.id,
+//    this.price,
+//    this.title,
+//    this.images,
+//    this.description,
+//    this.rating,
+// });
+// }
+//
+// List<product_details_page> product = [
+//   product_details_page(
+//       "id": "0",
+//       "price": "$ 120.00",
+//       "title": "DESTEK V5 VR Headset",
+//       "images": "https://i.postimg.cc/L6rzhPQj/1-Varjo-VR-3.png",
+//       "rating": 4.5,
+//       "description": "Comfortable & lightweight – Compare to market 3D VR headset, Our VR headset decreased in weight due to plastic component swaps and improvements in manufacturing. The virtual reality headset for kids only weights 9.9oz, it significantly reduce the pressure with the help of adjustable head strap design, making it tight but fairly comfortable fit. The removable facepad of VR head set with hand controllers is made of ultra-soft washable breathing Fabric, helping you stay comfortable during use",
+//   )
+//
+// ]
+
 class ProductDetailsPage extends StatefulWidget {
   const ProductDetailsPage({super.key});
 
