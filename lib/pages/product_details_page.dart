f7f143dab49faea49f181357f0503e407e29492b
Michila <michila.bimsara12@gmail.com>
2023-06-05 11:13:15 +0530

    creat the second page

---
+++ lib/pages/product_details_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Column(children: [
+        Row(children: const [
+          Text(
+            //display the name
+            ' Enjoy the world \n into Virtual reality',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              // decoration: TextDecoration.overline,
+              overflow: TextOverflow.fade,
+            ),
+          ),
+          //Notification
+
+        ],)
+      ]),
+    );
+  }
+}
